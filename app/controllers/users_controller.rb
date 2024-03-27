f546ceb21da5eced5c212c7c71f06703d93c064a
Jared McMahan <jaredm788@gmail.com>
2024-03-27 16:19:54 -0600

    User Create and Fallback

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+    # skip_before_action :authorize, only: [:create]
+    # show all users 
+    def index
+        render json: User.all
+    end
+    # create a user and test in postman
+    def create
+        user = User.create!(user_params)
+        render json: user, status: :created
+    end
+
+
+
+    private 
+    def user_params 
+        params.permit(:username, :password) 
+    end
 end
