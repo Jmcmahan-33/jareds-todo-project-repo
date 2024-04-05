066427b679c1443b2bd77f037458ca4f09d1aee7
Jared McMahan <jaredm788@gmail.com>
2024-04-05 07:07:09 -0600

    Fetch Me Route

--- client/src/context/user.js
+++ client/src/context/user.js
@@ -5,9 +5,25 @@ import React, { useState, useEffect } from "react";
 const UserContext = React.createContext();
 
 function UserProvider({ children }) {
-   
 
 
+
+    useEffect(() => {
+        fetch("/me")
+            // makes a request to the me route to get data from user. 
+            .then(res => res.json()) //converts the response to json
+            .then(data => {
+                if (data.errors) {
+                    console.log(data.errors)
+                } else {
+                    console.log(data)
+                }
+            })
+    })
+    //  I want to set up user state. 
+    // to use with  the data I get back from the server.
+    // if its not the user, set the error message.
+    // other wise set the user to the data.
 
     return (
         <UserContext.Provider>
